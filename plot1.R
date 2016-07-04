6ae5537bdf72db9c1b417f50b8f4f13e580e146e
andycheng171 <scheng30@jhu.edu>
2016-07-03 21:35:47 -0400

    add plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,17 @@
+# Subset the data by date 
 
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data_sub<- subset(data, subset=(data$Date>="2007-02-01" & data$Date<="2007-02-02"))
+rm(data)
+
+# Draw the plot 1
+
+data_sub$Global_active_power<-as.numeric(data_sub$Global_active_power)
+
+hist(data_sub$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red",col.bg="White")
+
+# Export the plot to PNG format
+
+dev.copy(png,file="plot1.png")
+dev.off()
